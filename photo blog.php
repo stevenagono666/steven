8b2c63a2e16fd6f7987c0db41bf0b7fd5f33d0f1
stevenagono666 <102359545+stevenagono666@users.noreply.github.com>
2022-03-25 18:55:43 +0800

    Add files via upload

---
+++ photo blog.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>
+		PORTFOLIO
+	</title>
+  <link rel="shortcut icon"  type="image/x-icon" sizes="18×18" href="images/logoss.png">
+</head>
+<body background="1.jpg" link="#000" alink="#017bf5" vlink="#000">
+
+    <table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="#f3971b">
+        <tr>
+             <td>
+                 <table border="0" width="85%" cellpadding="15" cellspacing="0" align="center">
+                 <tr>
+                        <td>
+                              <font face="arial" color="black" size="5">
+                                <img src="images/logoss.png" width="10%"/>
+                            <strong>SCIENCE & TECHNOLOGY.</strong>
+                          
+                           </font>
+                          
+                        </td>
+                        <td width="30%">&nbsp;</td>
+                        <td><a href="#home">
+                            <a href="index.html">HOME</a>
+                              <font face="arial" color="#ffffff" size="3">
+                           
+                          </font></a>
+                        </td>
+                        <td><a href="#Links">
+                      <a href="Links.html">LINKS</a>
+                                   <font face="arial" color="#ffffff" size="3">
+                           
+                             </font></a>
+                         </td>
+                         
+                        <td><a href="#Personal info">
+                      <a href="Personal.html">PERSONAL INFO</a>
+                            <font face="arial" color="#ffffff" size="3">
+                         
+                         </font></a>
+                        </td>
+                        <td><a href="#Written blog">
+                          <a href="Written.html">WRITTEN BLOG</a>
+                              <font face="arial" color="#ffffff" size="3">
+                             </font></a>
+                            </td>
+                        <td><a href="#portfolio">
+                      <a href="Portfolio.html">PORTFOLIO </a>
+                             <font face="arial" color="#ffffff" size="3">
+                        
+                       </font></a>
+                       </td>
+                       
+					  <td><a href="#OTHERS">
+              <a href="Others.html">OTHERS</a>
+                  <font face="arial" color="#ffffff" size="3">
+                 </font></a>
+                </td>
+                 </tr>
+                 </table>
+             </td>
+        </tr>
+      </table>
+      <!-- End Header -->
+      
+<!-- Start Portfolio -->
+<table border="0" id="portfolio" width="100%" cellpadding="0" cellspacing="0" bgcolor="#292929">
+  <tr>
+  	 <td>
+  	 	<table border="0" width="85%" cellpadding="15" cellspacing="0" align="center">
+  	 	<!-- Heading Start-->
+          <tr>
+           	  <td height="160" align="center" valign="middle" colspan="3">
+           	  	 <font face="arial" size="6" color=" #f3971b">
+           	  	   Portfolio
+           	  	</font>
+           	  	<hr width="70" color="#f3971b">
+           	  </td>
+          </tr>
+        <!-- Heading  End-->
+         <tr>
+         	<td width="33.33%" valign="top">
+         	  <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+         	  	<tr>
+         	  		<td>
+         	  		
+         	  		   <br/>
+         	  		   <br/>
+         	  		  
+         	  		   <br/><br/>
+                       <img src="images/port1.jpg" width="80%"/>
+         	  		</td>
+         	  	</tr>
+         	  </table>	
+         	</td>
+         	
+         	<td width="33.33%" valign="top">
+         	  <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+         	  	<tr>
+         	  		<td>
+         	  		
+         	  		   <br/>
+         	  		   <br/>
+         	  		 
+         	  		   <br/><br/>
+                       <img src="images/port2.jpg" width="100%"/>
+         	  		</td>
+         	  	</tr>
+         	  </table>	
+         	</td>
+
+         	<td width="33.33%" valign="top">
+         	  <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+         	  	<tr>
+         	  		<td>
+         	  		
+         	  		   <br/>
+         	  		   <br/>
+         	  		  
+         	  		   <br/><br/>
+                       <img src="images/port3.jpg" width="100%"/>
+         	  		</td>
+         	  	</tr>
+         	  </table>	
+         	</td>
+         </tr>
+          <tr>
+         	<td width="33.33%" valign="top">
+         	  <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+         	  	<tr>
+         	  		<td>
+         	  		
+         	  		   <br/>
+         	  		   <br/>
+         	  		  
+         	  		   <br/><br/>
+                       <img src="images/port4.jpg" width="100%"/>
+         	  		</td>
+         	  	</tr>
+         	  </table>	
+         	</td>
+         	
+         	<td width="33.33%" valign="top">
+         	  <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+         	  	<tr>
+         	  		<td>
+         	  			
+         	  		   <br/>
+         	  		   <br/>
+         	  		   <font face="arial" size="4" color="#f3971b">
+         	  			
+         	  		   </font>
+         	  		   <br/><br/>
+                       <img src="images/port5.jpg" width="100%"/>
+         	  		</td>
+         	  	</tr>
+         	  </table>	
+         	</td>
+
+         	<td width="33.33%" valign="top">
+         	  <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+         	  	<tr>
+         	  		<td>
+         	  			
+         	  		   <br/>
+         	  		   <br/>
+         	  		 
+         	  		   <br/><br/>
+                       <img src="images/port6.jpeg" width="100%"/>
+         	  		</td>
+         	  	</tr>
+         	  </table>	
+         	</td>
+         </tr>
+        <!-- section padding bottom -->
+           <tr>
+           	  <td height="60" colspan="3">
+                 
+           	  </td>
+           </tr>
+          <!-- section padding bottom End-->
+       </table>
+     </td>
+   </tr>
+</table>
+<!-- End Portfolio -->
+
+<!-- Start Footer -->
+<table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="#353535">
+  <tr>
+  	 <td>
+  	 	<table border="0" width="85%" cellpadding="15" cellspacing="0" align="center">
+  	 	
+  	 	<tr>
+  	 		<td width="33.33%" valign="top">
+  	 		   <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center">
+  	 		   	 <tr>
+  	 		   	 	<td align="center" valign="top">
+  	 		   	 		<font color="#f3971b" size="6">
+  	 		   	 			<span class="fa fa-phone"></span>
+  	 		   	 		</font>
+  	 		   	 		<br/><br/>
+  	 		   	 		<font color="#ffffff" size="5" face="arial">
+  	 		   	 		Phone No
+  	 		   	 	    </font>
+  	 		   	 	    <p>
+  	 		   	 	      <font color="#c2c0c3" size="3" face="arial">
+  	 		   	 	    	+91 9878666666
+  	 		   	 	     </font>
+  	 		   	 	    </p>
+  	 		   	 	</td>
+  	 		   	 </tr>
+  	 		   </table>
+  	 		</td>
+
+  	 		<td width="33.33%" valign="top">
+  	 		   <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center">
+  	 		   	 <tr>
+  	 		   	 	<td align="center" valign="top">
+  	 		   	 		<font color="#f3971b" size="6">
+  	 		   	 			<span class="fa fa-envelope"></span>
+  	 		   	 		</font>
+  	 		   	 		<br/><br/>
+  	 		   	 		<font color="#ffffff" size="5" face="arial">
+  	 		   	 		Email
+  	 		   	 	    </font>
+  	 		   	 	    <p>
+  	 		   	 	      <font color="#c2c0c3" size="3" face="arial">
+  	 		   	 	    	kentryanpitogo64@gmail.com
+  	 		   	 	     </font>
+  	 		   	 	    </p>
+  	 		   	 	</td>
+  	 		   	 </tr>
+  	 		   </table>
+  	 		</td>
+
+  	 		<td width="33.33%" valign="top">
+  	 		   <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center">
+  	 		   	 <tr>
+  	 		   	 	<td align="center" valign="top">
+  	 		   	 		<font color="#f3971b" size="6">
+  	 		   	 			<span class="fa fa-map-marker"></span>
+  	 		   	 		</font>
+  	 		   	 		<br/><br/>
+  	 		   	 		<font color="#ffffff" size="5" face="arial">
+  	 		   	 		Location
+  	 		   	 	    </font>
+  	 		   	 	    <p>
+  	 		   	 	      <font color="#c2c0c3" size="3" face="arial">
+  	 		   	 	    	Zone Pechay Paknaan
+  	 		   	 	    	Cebu City
+  	 		   	 	     </font>
+  	 		   	 	    </p>
+  	 		   	 	</td>
+  	 		   	 </tr>
+  	 		   </table>
+  	 		</td>
+  	 	</tr>
+  	 	<tr>
+  	 		<td colspan="3">
+  	 			<hr noshade>
+  	 		</td>
+  	 	</tr>
+  	 	<tr>
+  	 		<td colspan="3" height="100" valign="top">
+  	 			<table border="0" width="30%" cellpadding="0" cellspacing="0" align="center">
+  	 				
+  	 				<tr>
+  	 					 <td width="25%" align="center">
+  	 					 	<table border="0" width="50" cellpadding="0" cellspacing="0" align="center" height="50" bgcolor="#f3971b">
+  	 					 		<tr>
+  	 					 			<td align="center" valign="middle">
+  	 					 				<a href="">
+  	 					 				<font size="5" color="#ffffff">
+  	 					 				<span class="fa fa-facebook"></span>
+  	 					 			   </font>
+  	 					 			    </a>
+  	 					 			</td>
+  	 					 		</tr>
+  	 					 	
+  	 					 	</table>
+  	 					 </td>
+  	 					 <td width="25%" align="center">
+  	 					 	<table border="0" width="50" cellpadding="0" cellspacing="0" align="center" height="50" bgcolor="#f3971b">
+  	 					 		<tr>
+  	 					 			<td align="center" valign="middle">
+  	 					 				<a href="">
+  	 					 				<font size="5" color="#ffffff">
+  	 					 				<span class="fa fa-skype"></span>
+  	 					 			   </font>
+  	 					 			</a>
+  	 					 			</td>
+  	 					 		</tr>
+  	 					 	
+  	 					 	</table>
+  	 					 </td>
+  	 					 <td width="25%" align="center">
+  	 					 	<table border="0" width="50" cellpadding="0" cellspacing="0" align="center" height="50" bgcolor="#f3971b">
+  	 					 		<tr>
+  	 					 			<td align="center" valign="middle">
+  	 					 				<a href="">
+  	 					 				<font size="5" color="#ffffff">
+  	 					 				<span class="fa fa-linkedin"></span>
+  	 					 			   </font>
+  	 					 			   </a>
+  	 					 			</td>
+  	 					 		</tr>
+  	 					 	
+  	 					 	</table>
+  	 					 </td>
+  	 					 <td width="25%" align="center">
+  	 					 	<table border="0" width="50" cellpadding="0" cellspacing="0" align="center" height="50" bgcolor="#f3971b">
+  	 					 		<tr>
+  	 					 			<td align="center" valign="middle">
+  	 					 				<a href="">
+  	 					 				<font size="5" color="#ffffff">
+  	 					 				<span class="fa fa-google-plus"></span>
+  	 					 			   </font>
+  	 					 			   </a>
+  	 					 			</td>
+  	 					 		</tr>
+  	 					 	
+  	 					 	</table>
+  	 					 </td>
+  	 				</tr>
+  	 			</table>
+  	 		</td>
+  	 	</tr>
+  	 	</table>
+  	</td>
+  </tr>
+ </table>
+<!-- End Footer -->
+</body>
+</html>
